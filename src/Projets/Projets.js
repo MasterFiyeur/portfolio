e745dea5ee186b36cdcee81d36ac72f98ab0a43e
MasterFiyeur <julientheo@eisti.eu>
2020-07-05 01:35:45 +0200

    ressortir les textes Projets en large

--- src/Projets/Projets.js
+++ src/Projets/Projets.js
@@ -27,59 +27,79 @@ class Projets extends Component{
                 <div className="all_Projet_Projets">
                     <div className="liste_Projet_Projets">
                         <img className="img_large" src={img_site_personnel} alt="Site personnel"/>
-                        <div className="description_Projets d-none d-lg-block">
+                        <div className="description_large_Projets d-none d-lg-block">
                             <h1><span className="project_title">Portfolio</span> - Juillet 2020</h1>
-                            <div>Il s'agit de ce site web afin de me présenter de même que mes projets.</div>
-                            <div className="info_info-button_Projets"><div>Langages : HTML / CSS / JS (React)</div><div className="voir_Projets">Voir</div></div>
+                            <div className="description_text_Projets">Il s'agit de ce site web afin de me présenter de même que mes projets.</div>
+                            <div className="info_info-button_Projets"><div className="langages_lg_Projets">Langages : HTML / CSS / JS (React)</div>
+                            <div className="voir_Projets">Détails</div></div>
                         </div>
-                        <div className=" d-lg-none d-block">Je suis làààààà !!</div>
+                        <div className="description_nolarge_Projets d-lg-none d-block">
+                            <div className="row">
+                                <div className="col-12 col-sm-4">
+                                    <h1>Portfolio</h1>
+                                    <h4>Juillet 2020</h4>
+                                    <div>Langages : HTML / CSS / JS (React)</div>
+                                <hr className="d-md-none d-sm-none"></hr>
+                                </div>
+                                <div className="col-12 col-sm-8 description_centered_Projets">
+                                    <div>Il s'agit de ce site web afin de me présenter de même que mes projets.</div>
+                                    <div className="btn_voir_Projets">Details</div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div className="liste_Projet_Projets">
                         <img className="img_large" src={img_myeisti} alt="MyEISTI"/>
-                        <div className="description_Projets">
+                        <div className="description_large_Projets d-none d-lg-block">
                             <h1><span className="project_title">MyEISTI</span> - Juin 2020</h1>
-                            <div>Une application dont le but premier est de faciliter l'appel des professeurs en début de cours mais qui contient d'autres fonctionnalités.</div>
-                            <div className="info_info-button_Projets"><div>Langages : Java / PHP / SQL</div><div className="voir_Projets">Voir</div></div>
+                            <div className="description_text_Projets">Une application dont le but premier est de faciliter l'appel des professeurs en début de cours mais qui contient d'autres fonctionnalités.</div>
+                            <div className="info_info-button_Projets"><div className="langages_lg_Projets">Langages : Java / PHP / SQL</div>
+                            <div className="voir_Projets">Détails</div></div>
                         </div>
                     </div>
                     <div className="liste_Projet_Projets">
                         <img className="img_large" src={img_site_rencontre} alt="Site rencontre"/>
-                        <div className="description_Projets">
+                        <div className="description_large_Projets d-none d-lg-block">
                             <h1><span className="project_title">Site de rencontre</span> - Juin 2020</h1>
-                            <div>Un site internet qui propose aux utilisateurs de "like" ou "dislike" des profils et d'établir une communication entre deux utilisateurs s'ils se sont "like" mutuellement.</div>
-                            <div className="info_info-button_Projets"><div>Langages : HTML / CSS / JS (React) / PHP / SQL</div><div className="voir_Projets">Voir</div></div>
+                            <div className="description_text_Projets">Un site internet qui propose aux utilisateurs de "like" ou "dislike" des profils et d'établir une communication entre deux utilisateurs s'ils se sont "like" mutuellement.</div>
+                            <div className="info_info-button_Projets"><div className="langages_lg_Projets">Langages : HTML / CSS / JS (React) / PHP / SQL</div>
+                            <div className="voir_Projets">Détails</div></div>
                         </div>
                     </div>
                     <div className="liste_Projet_Projets">
                         <img className="img_large" src={img_bataille_naval} alt="Bataille navale"/>
-                        <div className="description_Projets">
+                        <div className="description_large_Projets d-none d-lg-block">
                             <h1><span className="project_title">Bataille navale</span> - Janvier 2020</h1>
-                            <div>Une bataille navale dont on peut changer quelques règles.</div>
-                            <div className="info_info-button_Projets"><div>Langage : C</div><div className="voir_Projets">Voir</div></div>
+                            <div className="description_text_Projets">Une bataille navale dont on peut changer quelques règles.</div>
+                            <div className="info_info-button_Projets"><div className="langages_lg_Projets">Langage : C</div>
+                            <div className="voir_Projets">Détails</div></div>
                         </div>
                     </div>
                     <div className="liste_Projet_Projets">
                         <img className="img_large" src={img_ascenseur} alt="Ascenseur"/>
-                        <div className="description_Projets">
+                        <div className="description_large_Projets d-none d-lg-block">
                             <h1><span className="project_title">Ascenseur</span> - Juin 2019</h1>
-                            <div>Un jeu de carte, c'est un jeu de levées avec atout.</div>
-                            <div className="info_info-button_Projets"><div>Langage : Pascal</div><div className="voir_Projets">Voir</div></div>
+                            <div className="description_text_Projets">Un jeu de carte, c'est un jeu de levées avec atout.</div>
+                            <div className="info_info-button_Projets"><div className="langages_lg_Projets">Langage : Pascal</div>
+                            <div className="voir_Projets">Détails</div></div>
                         </div>
                     </div>
                     <div className="liste_Projet_Projets">
                         <img className="img_large" src={img_machine_mot} alt="Machine à inventer des mots"/>
-                        <div className="description_Projets">
+                        <div className="description_large_Projets d-none d-lg-block">
                             <h1><span className="project_title">Machine à inventer des mots</span> - Janvier 2019</h1>
-                            <div>Invention de mots selon les probabilités des suites de lettres d'un dictionnaire.</div>
-                            <div className="info_info-button_Projets"><div>Langage : Pascal</div><div className="voir_Projets">Voir</div></div>
+                            <div className="description_text_Projets">Invention de mots selon les probabilités des suites de lettres d'un dictionnaire.</div>
+                            <div className="info_info-button_Projets"><div className="langages_lg_Projets">Langage : Pascal</div>
+                            <div className="voir_Projets">Détails</div></div>
                         </div>
                     </div>
                     <div className="liste_Projet_Projets">
                         <img className="img_large" src={img_monopoly} alt="Monopoly"/>
-                        <div className="description_Projets">
+                        <div className="description_large_Projets d-none d-lg-block">
                             <h1><span className="project_title">Monopoly</span> - Juin 2018</h1>
-                            <div>Un monopoly dans lequel nous pouvons acheter les salles de classe de mon lycée.</div>
-                            <div className="info_info-button_Projets"><div>Langages : HTML / CSS / JS</div><div className="voir_Projets">Voir</div></div>
+                            <div className="description_text_Projets">Un monopoly dans lequel nous pouvons acheter les salles de classe de mon lycée.</div>
+                            <div className="info_info-button_Projets"><div className="langages_lg_Projets">Langages : HTML / CSS / JS</div>
+                            <div className="voir_Projets">Détails</div></div>
                         </div>
                     </div>
                 </div>
