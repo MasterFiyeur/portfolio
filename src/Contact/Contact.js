8b8aa498d6ce7419626469f62dd22def693fea2a
MasterFiyeur <julientheo@eisti.eu>
2020-10-29 03:54:12 +0100

    Contact page

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -7,11 +7,58 @@ class Contact extends Component{
         super(props);
     }
     
+    openURL (url){
+        let open = window.open(url,'_blank');
+        open.focus();
+    }
+
     render(){
         return(
             <div className="body_Contact">
                 <BackArrow />
-                
+                <h1 className="title_Contact">Contact</h1>
+                <div className="Contact_icons">
+                    <div className="Contact_link" onClick={() => this.openURL("https://github.com/MasterFiyeur")}>
+                        <div className="layer">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span className="fab fa-github"></span>
+                        </div>
+                        <div className="link_text">GitHub</div>
+                    </div>
+                    <div className="Contact_link" onClick={() => this.openURL("https://www.linkedin.com/in/th%C3%A9o-julien-7888981ab/")}>
+                        <div className="layer">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span className="fab fa-linkedin-in"></span>
+                        </div>
+                        <div className="link_text">LinkedIn</div>
+                    </div>
+                    <div className="Contact_link" onClick={() => this.openURL("mailto:julientheo@eisti.eu")}>
+                        <div className="layer">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span className="far fa-envelope"></span>
+                        </div>
+                        <div className="link_text">julientheo@eisti.eu</div>
+                    </div>
+                    <div className="Contact_link" onClick={() => alert("Download CV")}>
+                        <div className="layer">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span className="far fa-file-pdf"></span>
+                        </div>
+                        <div className="link_text">CV</div>
+                    </div>
+                </div>
             </div>
         );
     }
