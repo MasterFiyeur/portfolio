fcdb7e9d60b342c9915cb6be7d36df3682e98616
Théo Julien <theo5julien@outlook.fr>
2020-11-02 17:52:10 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.4c0014c7b41eff8bce19be461ded95d1.js"
+  "/portfolio/precache-manifest.95b9467050c82851d0fcf164728bde36.js"
 );
 
 self.addEventListener('message', (event) => {
