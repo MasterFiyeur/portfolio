e38ec24372dcf2d7f3b3faf383324d5ef8c423e2
Théo Julien <theo5julien@outlook.fr>
2020-12-24 16:02:14 +0100

    Projets 2 par ligne >=1280px

--- src/Projets/Projets.js
+++ src/Projets/Projets.js
@@ -35,10 +35,12 @@ class Projets extends Component{
                     <div className="liste_Projet_Projets">
                         <img className="img_large" src={img_site_personnel} alt="Site personnel"/>
                         <div className="description_large_Projets d-none d-lg-flex">
-                            <h1><span className="project_title">Portfolio</span> - July 2020</h1>
+                            <h2><span className="project_title">Portfolio</span> - July 2020</h2>
                             <div className="description_text_Projets">Website in the purpose to introduce myself with my projects.</div>
-                            <div className="info_info-button_Projets"><div className="langages_lg_Projets">Languages : HTML / CSS / JS (React)</div>
-                            <div className="voir_Projets" onClick={() => this.openURL("https://github.com/MasterFiyeur/portfolio")}>GitHub</div></div>
+                            <div className="info_info-button_Projets">
+                                <div className="langages_lg_Projets">Languages : HTML / CSS / JS (React)</div>
+                                <div className="voir_Projets" onClick={() => this.openURL("https://github.com/MasterFiyeur/portfolio")}>GitHub</div>
+                            </div>
                         </div>
                         <div className="description_nolarge_Projets d-lg-none d-block">
                             <div className="row">
@@ -58,10 +60,12 @@ class Projets extends Component{
                     <div className="liste_Projet_Projets">
                         <img className="img_large" src={img_myeisti} alt="MyEISTI"/>
                         <div className="description_large_Projets d-none d-lg-flex">
-                            <h1><span className="project_title">MyEISTI</span> - June 2020</h1>
+                            <h2><span className="project_title">MyEISTI</span> - June 2020</h2>
                             <div className="description_text_Projets">An application firstly dedicated to facilitate the call roll of teachers at the beginning of the course, but which also contains other features.</div>
-                            <div className="info_info-button_Projets"><div className="langages_lg_Projets">Languages : Java / PHP / SQL</div>
-                            <div className="voir_Projets" onClick={() => this.openURL("https://github.com/MasterFiyeur/MyEistiApp")}>GitHub</div></div>
+                            <div className="info_info-button_Projets">
+                                <div className="langages_lg_Projets">Languages : Java / PHP / SQL</div>
+                                <div className="voir_Projets" onClick={() => this.openURL("https://github.com/MasterFiyeur/MyEistiApp")}>GitHub</div>
+                            </div>
                         </div>
                         <div className="description_nolarge_Projets d-lg-none d-block">
                             <div className="row">
@@ -81,10 +85,12 @@ class Projets extends Component{
                     <div className="liste_Projet_Projets">
                         <img className="img_large" src={img_site_rencontre} alt="Site rencontre"/>
                         <div className="description_large_Projets d-none d-lg-flex">
-                            <h1><span className="project_title">Dating site</span> - June 2020</h1>
+                            <h2><span className="project_title">Dating site</span> - June 2020</h2>
                             <div className="description_text_Projets">A website that offers to users to like or dislike profiles and to establish communication between two users if they like each other.</div>
-                            <div className="info_info-button_Projets"><div className="langages_lg_Projets">Languages : HTML / CSS / JS (React) / PHP / SQL</div>
-                            <div className="voir_Projets" onClick={() => this.openURL("https://github.com/MasterFiyeur/meeting-app-for-students")}>GitHub</div></div>
+                            <div className="info_info-button_Projets">
+                                <div className="langages_lg_Projets">Languages : HTML / CSS / JS (React) / PHP / SQL</div>
+                                <div className="voir_Projets" onClick={() => this.openURL("https://github.com/MasterFiyeur/meeting-app-for-students")}>GitHub</div>
+                            </div>
                         </div>
                         <div className="description_nolarge_Projets d-lg-none d-block">
                             <div className="row">
@@ -104,10 +110,12 @@ class Projets extends Component{
                     <div className="liste_Projet_Projets">
                         <img className="img_large" src={img_bataille_naval} alt="Bataille navale"/>
                         <div className="description_large_Projets d-none d-lg-flex">
-                            <h1><span className="project_title">Battleship</span> - January 2020</h1>
+                            <h2><span className="project_title">Battleship</span> - January 2020</h2>
                             <div className="description_text_Projets">A battleship game whose rules can be changed.</div>
-                            <div className="info_info-button_Projets"><div className="langages_lg_Projets">Language : C</div>
-                            <div className="voir_Projets" onClick={() => this.openURL("https://github.com/MasterFiyeur/battleships")}>GitHub</div></div>
+                            <div className="info_info-button_Projets">
+                                <div className="langages_lg_Projets">Language : C</div>
+                                <div className="voir_Projets" onClick={() => this.openURL("https://github.com/MasterFiyeur/battleships")}>GitHub</div>
+                            </div>
                         </div>
                         <div className="description_nolarge_Projets d-lg-none d-block">
                             <div className="row">
@@ -127,10 +135,12 @@ class Projets extends Component{
                     <div className="liste_Projet_Projets">
                         <img className="img_large" src={img_ascenseur} alt="Ascenseur"/>
                         <div className="description_large_Projets d-none d-lg-flex">
-                            <h1><span className="project_title">Ascenseur</span> - June 2019</h1>
+                            <h2><span className="project_title">Ascenseur</span> - June 2019</h2>
                             <div className="description_text_Projets">A card game where you can bet how many rounds you will win.</div>
-                            <div className="info_info-button_Projets"><div className="langages_lg_Projets">Language : Pascal</div>
-                            <div className="voir_Projets" onClick={() => this.openURL("https://github.com/MasterFiyeur/ascenseur")}>GitHub</div></div>
+                            <div className="info_info-button_Projets">
+                                <div className="langages_lg_Projets">Language : Pascal</div>
+                                <div className="voir_Projets" onClick={() => this.openURL("https://github.com/MasterFiyeur/ascenseur")}>GitHub</div>
+                            </div>
                         </div>
                         <div className="description_nolarge_Projets d-lg-none d-block">
                             <div className="row">
@@ -150,10 +160,12 @@ class Projets extends Component{
                     <div className="liste_Projet_Projets">
                         <img className="img_large" src={img_machine_mot} alt="Machine à inventer des mots"/>
                         <div className="description_large_Projets d-none d-lg-flex">
-                            <h1><span className="project_title">Word making machine</span> - January 2019</h1>
+                            <h2><span className="project_title">Word making machine</span> - January 2019</h2>
                             <div className="description_text_Projets">Invention of words according to the probabilities of the sequences of letters in a dictionary.</div>
-                            <div className="info_info-button_Projets"><div className="langages_lg_Projets">Language : Pascal</div>
-                            <div className="voir_Projets" onClick={() => this.openURL("https://github.com/MasterFiyeur/inventing-machine")}>GitHub</div></div>
+                            <div className="info_info-button_Projets">
+                                <div className="langages_lg_Projets">Language : Pascal</div>
+                                <div className="voir_Projets" onClick={() => this.openURL("https://github.com/MasterFiyeur/inventing-machine")}>GitHub</div>
+                            </div>
                         </div>
                         <div className="description_nolarge_Projets d-lg-none d-block">
                             <div className="row">
@@ -173,10 +185,12 @@ class Projets extends Component{
                     <div className="liste_Projet_Projets">
                         <img className="img_large" src={img_monopoly} alt="Monopoly"/>
                         <div className="description_large_Projets d-none d-lg-flex">
-                            <h1><span className="project_title">Monopoly</span> - June 2018</h1>
+                            <h2><span className="project_title">Monopoly</span> - June 2018</h2>
                             <div className="description_text_Projets">A monopoly in which we can buy the classrooms of my high school.</div>
-                            <div className="info_info-button_Projets"><div className="langages_lg_Projets">Languages : HTML / CSS / JS</div>
-                            <div className="voir_Projets" onClick={() => this.openURL("https://github.com/MasterFiyeur/monopoly")}>GitHub</div></div>
+                            <div className="info_info-button_Projets">
+                                <div className="langages_lg_Projets">Languages : HTML / CSS / JS</div>
+                                <div className="voir_Projets" onClick={() => this.openURL("https://github.com/MasterFiyeur/monopoly")}>GitHub</div>
+                            </div>
                         </div>
                         <div className="description_nolarge_Projets d-lg-none d-block">
                             <div className="row">
