9d039c2a9be35585ef652c899eda388916b0f084
Théo Julien <theo5julien@outlook.fr>
2022-08-27 14:13:39 +0200

    [CHG] updating CV

--- src/APropos/APropos.js
+++ src/APropos/APropos.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './APropos.css';
 import BackArrow from '../BackArrow/BackArrow';
-import CV_PDF_FR from './Theo_JULIEN_FR_CV.pdf';
+import CV_PDF_FR from './Theo_JULIEN_CV_FR.pdf';
 import CV_PDF_US from './Theo_JULIEN_CV_US.pdf';
 import cv_icon_fr from './CV_fr_icon.png';
 import cv_icon_us from './CV_us_icon.png';
@@ -38,7 +38,7 @@ class APropos extends Component{
                 <div className="bannerText">
                     <div className={this.state.paragraphe==0?"active":""}>
                         <h2>My life's summary</h2>
-                        <p>My name is Théo Julien, I'm 20 years old and I'm currently in my first year of
+                        <p>My name is Théo Julien, I'm 21 years old and I'm currently in my third year of
                         engineering school in computer science. I'm from Orléans but I moved house to Cergy for ice skating.
                         In my second year in high school I decided to stop figure skating because 
                         high level sport requires a lot of sacrifices and I had already enjoyed it a lot so I focused on studies and 
@@ -49,7 +49,7 @@ class APropos extends Component{
                         EISTI was my first choice because there was more computer science in the program than other schools and I was 
                         already passionate about computer science at that time. These two years were difficult, but I also enjoyed
                         them a lot because at the end of each semester we had an IT project to do. Validating my preparatory class 
-                        in June 2020, I am now in the first year of the engineering cycle at CY Tech (ex-EISTI) so in my third year after the baccalaureate.</p>
+                        in June 2020, I am now in the third year of the engineering cycle at CY Tech (ex-EISTI) so in my five year after the baccalaureate.</p>
                     </div>
                     <div className={this.state.paragraphe==1?"active":""}>
                         <h2>IT in my life</h2>
@@ -75,10 +75,10 @@ class APropos extends Component{
                         <h2>More about me...</h2>
                         <p>There are a lot of things I do at the same time of school. I am a former high level figure skating athlete, I was twice French 
                         champion and have travelled to Hungary, Latvia, Italy and Spain to do competitions. Currently I am no longer figure skating but 
-                        I do coach groups of skaters and I judge a few competitions. Since September 2019 I am an ambassador of my school, I represent 
+                        I do coach groups of skaters and I judge a few competitions. From 2020 to 2022 I was an ambassador of my school, I represented 
                         it at student fairs and I present the school at open days. I am involved as an ambassador because I am proud of my school and 
-                        I like the social contact with other people who want to discover my school. And since this year I am a tutor at Parkours, I 
-                        go to middle schools to help people with their homeworks in small groups (usually 5 students). As you will have understood, 
+                        I like the social contact with other people who want to discover my school. From 2020 to 2021 I was a tutor at Parkours, I 
+                        went to middle schools to help people with their homeworks in small groups (usually 5 students). As you will have understood, 
                         this activity is very pleasant for me because I love to pass on my knowledge and intellectual curiosity.<br/>
                         Here is a video of me doing figure skating : 
                             <div className="iframe_patinage">
@@ -89,7 +89,7 @@ class APropos extends Component{
                 </div>
                 <ul className="sci">
                     <li><div onClick={() => window.open(CV_PDF_FR,'_blank').focus()}><img src={cv_icon_fr} alt="CV FR"/></div></li>
-                    <li><div onClick={() => window.open(CV_PDF_US,'_blank').focus()}><img src={cv_icon_us} alt="CV EN"/></div></li>
+                   {/*<li><div onClick={() => window.open(CV_PDF_US,'_blank').focus()}><img src={cv_icon_us} alt="CV EN"/></div></li>*/}
                 </ul>
 
                 <ul className="controls">
